913a0e664459555cd8e201d45f39956d5945678e
Marzuk Rashid <mail@marzuk.io>
2022-11-06 12:49:26 -0500

    updated source

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./site/**/*.html'],
+  theme: {
+    fontFamily: {
+      sans: '\'Lexend Deca\', sans-serif',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
