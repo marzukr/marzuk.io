444bc4bdbb05be6e9c051be8deee04f9707d9aca
Marzuk Rashid <marzuk.rashid@torchdental.com>
2023-08-17 08:42:08 -0500

    changes

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # marzukr.io
 
 This is my personal website. I made it myself.
+
+## Building
+
+```
+sudo bundle install
+npm install
+sudo npm run start
+```
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
   theme: {
     fontFamily: {
       sans: '\'Lexend Deca\', sans-serif',
+      serif: '\'Lora\', serif',
     },
     extend: {},
   },
