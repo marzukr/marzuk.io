3a30946ec6080a999ba0a097caf7e50d0268a4c5
Marzuk Rashid <mail@marzuk.io>
2025-03-17 11:37:37 -0500

    simplify

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,37 +1,11 @@
-<script lang="ts">
-  import Link from './Link.svelte';
-</script>
-
 <svelte:head>
-  <title>@realmarzuk</title>
+  <title>Marzuk</title>
   <meta
     name="description"
-    content="Marzuk is a former engineer currently exploring comedy, storytelling and social charisma."
+    content="Marzuk is a human living on planet earth."
   />
 </svelte:head>
 
-<div class="mx-auto max-w-3xl overflow-hidden p-6 pb-12 text-base">
-  <div class="mt-2 flex">
-    <img
-      src="/profile.jpg"
-      class="
-        mr-4 h-[7.5rem] rounded-md min-[690px]:h-[8.25rem]
-        min-[708px]:h-[8.5rem]
-      "
-      alt="Marzuk Rashid"
-    />
-    <div>
-      <p>MARZUK</p>
-      <p class="mt-2 font-serif">
-        Broke up with engineering for a fling with comedy. Might beg her to take
-        me back later.
-      </p>
-      <p class="mt-2 font-serif">
-        <Link href="https://www.instagram.com/realmarzuk/">instagram</Link>,
-        <Link href="https://www.youtube.com/@realmarzuk">youtube</Link>,
-        <Link href="https://www.tiktok.com/@realmarzuk">tiktok</Link>,
-        <Link href="mailto:hello@marzuk.io">email</Link>
-      </p>
-    </div>
-  </div>
+<div class="flex w-screen h-screen bg-black items-center justify-center">
+  <p class="text-white">MARZUK</p>
 </div>
